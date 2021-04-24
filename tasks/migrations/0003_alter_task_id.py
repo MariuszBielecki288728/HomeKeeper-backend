261ea0da9eead3919e887468d17a81d1fce5b1cf
Mariusz Bielecki <maniekb12@gmail.com>
2021-04-24 19:59:33 +0200

    Format migration

--- tasks/migrations/0003_alter_task_id.py
+++ tasks/migrations/0003_alter_task_id.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0002_auto_20201230_0417'),
+        ("tasks", "0002_auto_20201230_0417"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='task',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="task",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
