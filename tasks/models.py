c606aa6a1173d7d9d7a1a5784060ca75b5c7df91
Mariusz Bielecki <maniekb12@gmail.com>
2021-06-05 03:45:25 +0200

    Add a mechanism to increase task reward over time

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
+import datetime
+import math
+
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -5,6 +8,8 @@ from django.utils.timezone import now
 
 from teams.models import Team
 from common.models import TrackingFieldsMixin
+
+POINTS_INCREASE_INTERVAL = 7
 
 
 class Task(TrackingFieldsMixin):
@@ -54,8 +59,19 @@ class TaskInstance(TrackingFieldsMixin):
         """
         return self.completed is False and self.active_from <= now() and super().active
 
-    def calculate_prize(self) -> int:
-        return self.task.base_points_prize
+    @property
+    def current_prize(self) -> int:
+        """Calculates current value of the points reward for task instance completion.
+
+        In order to encourage users to complete stale tasks, the current prize
+        increase over time. For recurring tasks, base points prize is multiplied
+        by the number of times that interval has passed twice. For single tasks,
+        the constant is taken as interval - currently 7 days.
+        """
+        multiplication_interval = self.task.refresh_interval or datetime.timedelta(
+            days=POINTS_INCREASE_INTERVAL
+        )
+        return math.ceil((now() - self.active_from) / (multiplication_interval * 2))
 
 
 class TaskInstanceCompletion(TrackingFieldsMixin):
@@ -67,3 +83,15 @@ class TaskInstanceCompletion(TrackingFieldsMixin):
     user_who_completed_task = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.PROTECT
     )
+    points_granted = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        if self._state.adding is True:
+            self.grant_points_prize()
+        super().save(*args, **kwargs)
+
+    def grant_points_prize(self):
+        """
+        Sets current reward of the task as granted points. Does not save.
+        """
+        self.points_granted = self.task_instance.current_prize
