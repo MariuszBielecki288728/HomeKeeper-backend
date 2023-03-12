a11f87cd4195a521dabee0d1a03c4b42fd75e972
Mariusz Bielecki <mariuszbielecki01@gmail.com>
2023-03-12 21:24:09 +0100

    Cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-
-# pwd && ls -al && source /venv/bin/activate
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
